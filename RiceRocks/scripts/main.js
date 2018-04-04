e5ff90152b891b5caa218ba4d0605b1c72c30dcc
Kyle <k.yasumiishi@gmail.com>
2018-04-04 14:26:16 -0700

    Updated start screen

--- RiceRocks/scripts/main.js
+++ RiceRocks/scripts/main.js
@@ -80,7 +80,7 @@ var explosion_info = new ImageInfo([64, 64], [128, 128], 17, 24, true);
 // Draw canvas when page loads
 
 function load_canvas() {
-    draw();
+    setInterval(function() {draw()}, 10);
 
 }
 
@@ -94,63 +94,206 @@ function dist(p, q) {
 }
 
 // Ship class
-// class Ship {
-//     constructor(pos, vel, angle, image, info) {
-//         this.pos = [pos[0], pos[1]];
-//         this.vel = [vel[0], vel[1]];
-//         this.thrust = false;
-//         this.angle = angle;
-//         this.angle_vel = 0;
-//         this.image = image;
-//         this.image_center = info.get_center();
-//         this.radius = info.get_radius();
-//         this.info = info;
-//     }
-//     get_position() {
-//         return this.pos;
-//     }
-//     get_radius() {
-//         return this.radius;
-//     }
-//     draw(canvas) {
-//         if (this.thrust) {
-//             let thrust_image_center = (this.info.center[0] + this.info.size[0], this.info.center[1]);
-//             ////////////
-//         }
-//     }
-// }
+class Ship {
+    constructor(pos, vel, angle, info) {
+        this.pos = [pos[0], pos[1]];
+        this.vel = [vel[0], vel[1]];
+        this.thrust = false;
+        this.angle = angle;
+        this.angle_vel = 0;
+        this.image_size = info.get_size();
+        this.radius = info.get_radius();
+        this.info = info;
+    }
+    get_position() {
+        return this.pos;
+    }
+    get_radius() {
+        return this.radius;
+    }
+    draw(img, context) {
+        context.save();
+        context.translate(this.pos[0] + this.image_size[0] / 2, this.pos[1] + this.image_size[1] / 2);
+        context.rotate(this.angle);
+        if (this.thrust) {
+            // context.drawImage(img, this.image_size[0], 0, this.image_size[0], this.image_size[1], this.pos[0], this.pos[1], this.image_size[0], this.image_size[1]);
+            context.drawImage(img, this.image_size[0], 0, this.image_size[0], this.image_size[1], -this.image_size[0] / 2, -this.image_size[1] / 2, this.image_size[0], this.image_size[1]);
+        } else {
+            // context.drawImage(img, 0, 0, this.image_size[0], this.image_size[1], this.pos[0], this.pos[1], this.image_size[0], this.image_size[1]);
+            context.drawImage(img, 0, 0, this.image_size[0], this.image_size[1], -this.image_size[0] / 2, -this.image_size[1] / 2, this.image_size[0], this.image_size[1]);
+        }
+        context.restore();
+    }
+    update() {
+        // Position update
+        if (this.pos[0] + this.vel[0] < 0) { 
+            this.pos[0] += WIDTH;
+        } else {
+            this.pos[0] = (this.pos[0] + this.vel[0]) % WIDTH;
+        }
+        if (this.pos[1] + this.vel[1] < 0) {
+            this.pos[1] += HEIGHT;
+        } else {
+            this.pos[1] = (this.pos[1] + this.vel[1]) % HEIGHT;
+         }
 
 
-// function draw() {
-//     var canvas = document.getElementById("frame");
-//     var ctx = canvas.getContext("2d");
-
-//     var nebula_image = new Image();
-//     nebula_image.src = "..images/nebula_blue.png"
-//     nebula_image.addEventListener("load", function() {ctx.drawImage(nebula_image), 0, 0}, false);
-
-    // Animate background
-  // ctx.drawImage(nebula_image, 0, 0);
-// }
 
 
-var canvas = document.getElementById("frame");
-var ctx = canvas.getContext("2d");
+        // this.pos[0] = (this.pos[0] + this.vel[0]) % WIDTH;
+        // this.pos[1] = (this.pos[1] + this.vel[1]) % HEIGHT;
 
-function doFirst() {
-    var img = new Image();
-    img.onload = function() {ctx.drawImage(img, 0, 0)}
-    img.src = "../images/nebula_blue.png"
+        // Friction update
+        let c = 0.03;
+        this.vel[0] *= (1 - c);
+        this.vel[1] *= (1 - c);
+
+        // Thrust update
+        let forward = angle_to_vector(this.angle);
+        if (this.thrust) {
+            this.vel[0] += forward[0] * 0.25;
+            this.vel[1] += forward[1] * 0.25;
+        } 
+
+        // Ship orientation update
+        this.angle += this.angle_vel;
+    }
 }
 
 
-doFirst();
+var my_ship = new Ship([(WIDTH / 2) - (45 / 2), (HEIGHT / 2) - (45 / 2)], [0, 0], 0, ship_info);
 
 
 function draw() {
     var canvas = document.getElementById("frame");
     var ctx = canvas.getContext("2d");
-    var nebula_image = new Image();
-    nebula_image.src = "images/nebula_blue.png";
+    
+    // Animate background
+    time++;
+    let wtime = (time / 4) % WIDTH;
+    let center = debris_info.get_center();
+    let size = debris_info.get_size();
+    var nebula_image = document.getElementById("nebula");
+    var debris_image = document.getElementById("debris");
     ctx.drawImage(nebula_image, 0, 0);
+    ctx.drawImage(debris_image, 0, 0, debris_info.get_size()[0], debris_info.get_size()[1], (wtime - WIDTH / 2), 60, debris_info.get_size()[0], debris_info.get_size()[1]);
+    ctx.drawImage(debris_image, 0, 0, debris_info.get_size()[0], debris_info.get_size()[1], (wtime + WIDTH / 2), 60, debris_info.get_size()[0], debris_info.get_size()[1]);
+
+    // Draw lives and scores
+    ctx.font = "25px Arial";
+    ctx.fillStyle = "white";
+    ctx.fillText("Lives", (WIDTH * 0.05), (HEIGHT * 0.08));
+    ctx.fillText(lives.toString(), (WIDTH * 0.05), (HEIGHT * 0.12));
+    ctx.fillText("Score", (WIDTH * 0.88), (HEIGHT * 0.08));
+    ctx.fillText(score.toString(), (WIDTH * 0.88), (HEIGHT * 0.12));
+
+    // Draw ship
+    var ship_image = document.getElementById("ship");
+    my_ship.draw(ship_image, ctx);
+
+    if (started) {
+        my_ship.update();
+    } else {
+        var splash_image = document.getElementById("splash");
+        ctx.drawImage(splash_image, 0, 0, splash_info.get_size()[0], splash_info.get_size()[1], 200, 150, splash_info.get_size()[0], splash_info.get_size()[1]);
+    }  
 }
+
+
+
+// Initialize mouse and keyboard handlers
+
+document.addEventListener("keydown", keydown);
+document.addEventListener("keyup", keyup);
+document.addEventListener("click", click);
+
+var keys = {
+    up: false,
+    down: false,
+    left: false,
+    right: false,
+    space: false
+};
+
+function keydown(event) {
+    switch(event.key) {
+        case "ArrowUp":
+            keys.up = true;
+            break;
+        case "ArrowDown":
+            keys.down = true;
+            break;
+        case "ArrowLeft":
+            keys.left = true;
+            break;
+        case "ArrowRight":
+            keys.right = true;
+            break;
+        case " ":
+            keys.space = true;
+            break;
+    }
+    move_handler();
+}
+
+function keyup(event) {
+    switch(event.key) {
+        case "ArrowUp":
+        keys.up = false;
+        break;
+    case "ArrowDown":
+        keys.down = false;
+        break;
+    case "ArrowLeft":
+        keys.left = false;
+        break;
+    case "ArrowRight":
+        keys.right = false;
+        break;
+    case " ":
+        keys.space = false;
+        break;
+    }
+    move_handler();
+}
+
+function move_handler() {
+    // left and right arrow keys
+    if ((keys.left && keys.right) || (!keys.left && !keys.right)) {
+        my_ship.angle_vel = 0;
+    } else if (keys.left && !keys.right) {
+        my_ship.angle_vel -= 0.075;
+    } else if (!keys.left && keys.right) {
+        my_ship.angle_vel += 0.075;
+    }
+    // up arrow key
+    if (keys.up) {
+        my_ship.thrust = true;
+    } else {
+        my_ship.thrust = false;
+    }
+    // space bar
+    if (keys.space) {
+        my_ship.shoot();
+    }
+}
+
+function click(event) {
+    // Resets UI and determines whether splash image is drawn.
+    let canvas = document.getElementById("frame");
+    x = event.clientX - canvas.offsetLeft;
+    y = event.clientY - canvas.offsetTop;
+    score = 0;
+    lives = 3;
+    let center = [WIDTH / 2, HEIGHT / 2];
+    let size = splash_info.get_size();
+    let inwidth = ((center[0] - (size[0] / 2) < x) && (x < center[0] + size[0] / 2));
+    let inheight = ((center[1] - (size[1] / 2) < y) && (y < center[1] + size[1] / 2));
+    // soundtrack.play();
+    if (!started && inwidth && inheight) {
+        started = true;
+        // timer.start();
+    }
+}
+
+
--- RiceRocks/scripts/test.js
+++
@@ -1,9 +0,0 @@
-function cool() {
-    var test = document.createElement("img");
-    test.src = "images/asteroid_blue.png";
-    var canvas = document.getElementById("frame");
-    var ctx = canvas.getContext("2d");
-    ctx.drawImage(test, 0, 0);
-}
-
-
